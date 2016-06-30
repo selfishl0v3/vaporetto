a7781c0935f27c0496f1375c8c1b37f2c551bc30
Alessio Zappa <alessioedoardozappa@gmail.com>
2016-06-30 19:44:29 +0200

    updating modules

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,4 @@
 
 ./node_modules/.bin/jade $(pwd)/*.jade
 ./node_modules/.bin/riot --template jade --compact $(pwd)/tags $(pwd)/dist/vaporetto.js
-./node_modules/.bin/stylus -c --use $(pwd)/node_modules/nib $(pwd)/static/css/style.styl
+./node_modules/.bin/stylus -c --use $(pwd)/node_modules/nib --use $(pwd)/node_modules/rupture $(pwd)/static/css/style.styl
